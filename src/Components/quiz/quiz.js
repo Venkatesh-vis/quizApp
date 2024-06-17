40d26577d2024c37c3edca6e9955c6419bb1dc32
Venkatesh-vis <venkateshvishwanadula257@gmail.com>
2024-06-17 10:32:40 +0530

    updated

--- src/Components/quiz/quiz.js
+++ src/Components/quiz/quiz.js
@@ -1,13 +1,72 @@
+import { useEffect, useState } from "react"
+import useSound from "use-sound"
+import play from "../../Assets/src_sounds_play.mp3"
+import correct from "../../Assets/src_sounds_correct.mp3"
+import wrong from "../../Assets/src_sounds_wrong.mp3"
 
- const Quiz = () => {
+ const Quiz = ({
+  data,
+  setTime,
+  setQuestionNumber,
+  questionNumber
+ }) => {
+  const[question,setQuestion]=useState(null)
+  const[selectedAnswer,setSelectedAnswer]=useState(null)
+  const[classname,setClassname]=useState(null)
+  const[letsPlay]=useSound(play)
+  const[correctAnswer]= useSound(correct)
+  const[wrongAnswer]= useSound(wrong)
+
+  useEffect(()=>{
+    letsPlay()
+  },[letsPlay])
+
+  useEffect(()=>{
+    setQuestion(data[questionNumber-1])
+  },[data,questionNumber])
+
+  const delay = (duration,callback) => {
+    setTimeout(() => {
+      callback()
+    }, duration);
+  }
+
+  const handleClick = (a) => {
+    setSelectedAnswer(a)
+    setClassname("answer active")
+    delay(3000,()=>{
+      setClassname(a.correct ? "answer correct" : "answer wrong")
+    });
+    delay(5000,()=>{
+        if(a.correct){
+          correctAnswer()
+          delay(1000, ()=>{
+            setQuestionNumber((prev)=>prev+1)
+          setSelectedAnswer(null)
+          })
+          
+        }
+        else{
+          wrongAnswer()
+          delay(1000,()=>{
+            setTime(true)
+          })
+          
+        }
+    });
+  }
+
     return(
       <div className="quiz">
-        <div className="question">whats my name</div>
+        <div className="question">{question?.question}</div>
         <div className="answers">
-            <div className="answer wrong">venkatesh</div>
-            <div className="answer">venkatesh</div>
-            <div className="answer">venkatesh</div>
-            <div className="answer">venkatesh</div>
+          {
+            question?.answers.map((a)=>(
+              <>
+              <div className={selectedAnswer === a? classname : "answer"} onClick={()=>handleClick(a)}>{a.text}</div>
+              </>
+            ))
+          }
         </div>
       </div>
     )
